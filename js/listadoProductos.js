518c930f81c28befa97c3a56d5910414d3cfde00
barakei-26 <barake883@gmail.com>
2024-01-28 22:26:37 -0500

    tarea prodcutos Rudy Juliana

--- js/listadoProductos.js
+++ js/listadoProductos.js
@@ -1,152 +1,19 @@
 // Define los productos de café por galería
 var cafeProducts = {
-    TodosLosProductos: [
+    gallery1: [
         { name: 'Product 1', imgSrc: 'img/gallery-1.jpg', description: 'Descripción del producto 1' },
         { name: 'Product 2', imgSrc: 'img/gallery-2.jpg', description: 'Descripción del producto 2' },
         // Agrega más productos de la galería 1 aquí
     ],
-    Cafés: [
-        { 
-            name: 'Café Mesa de los Santos',
-            imgSrc: 'Proyecto-Final/img/Productos/Mesa.png',
-            description: 'Granos de café de alta calidad, tipo arabica, listos para moler y preparar la taza perfecta de café en casa. Disfruta de la frescura y el sabor auténtico.',
-            price: '$25.000'
-        },
-        { 
-            name: 'Café Premium "Arábica San Alberto"', 
-            imgSrc: 'Proyecto-Final/img/Productos/Arabica.png', 
-            description: 'Disfruta de la elegancia y el sabor refinado. Proveniente de las mejores cosechas, cada grano es seleccionado con cuidado para ofrecerte una experiencia única con notas florales, cuerpo sedoso y un toque de avellana.', 
-            price: '$35.000' 
-        },
-        { 
-            name: 'Mezcla Intensa "Q.Coffee"', 
-            imgSrc: 'Proyecto-Final/img/Productos/Q.png', 
-            description: 'Sumérgete en la intensidad de nuestro café. Esta mezcla única ofrece un perfil de sabor audaz y fuerte, con notas de cacao y un toque ahumado. Ideal para aquellos que buscan una experiencia de café potente y llena de carácter.', 
-            price: '$30.000' 
-        },
-        { 
-            name: 'Café Descafeinado "Aroma De Occidente"', 
-            imgSrc: 'Proyecto-Final/img/Productos/Aroma.png',
-            description: 'Experimenta la tranquilidad con nuestro café descafeinado. Una mezcla suave y equilibrada, perfecta para disfrutar en cualquier momento del día. Conserva el delicioso sabor del café sin cafeína, con notas de almendra y un toque sutil de chocolate.', 
-            price: '$45.000' 
-        }
-
+    gallery2: [
+        { name: 'Product 3', imgSrc: 'img/gallery-3.jpg', description: 'Descripción del producto 3' },
+        { name: 'Product 4', imgSrc: 'img/gallery-4.jpg', description: 'Descripción del producto 4' },
         // Agrega más productos de la galería 2 aquí
     ],
-
-    Accesorios: [
-        { 
-            name: 'Aeropress (4 Tazas)', 
-            imgSrc: 'Proyecto-Final/img/Productos/Aeropress.png',
-            description: 'Innovadora herramienta para preparar café que combina la simplicidad y la calidad. Su diseño portátil la hace perfecta para llevarla contigo y disfrutar de tu café favorito en cualquier lugar.',
-            price: '$160.000'
-        },
-        { 
-            name: 'Mini Capuchinera',
-            imgSrc: 'Proyecto-Final/img/Productos/Capuccinera.png',
-            description: 'Una máquina compacta para preparar diferentes tipos de café. Ofrece la comodidad de disfrutar de un café de calidad de barista sin salir de casa.',
-            price: '$550.000'
-        },
-        { 
-            name: 'Chemex Clásica (6 Tazas)',
-            imgSrc: 'Proyecto-Final/img/Productos/Chemex.png',
-            description: 'Elegante cafetera de vidrio para preparar café mediante el método de goteo. Su diseño único y la calidad del café que produce la convierten en una opción popular entre los amantes del café.',
-            price: '$320.000'
-        },
-        { 
-            name: 'Vaso Térmico',
-            imgSrc: 'Proyecto-Final/img/Productos/Vaso.png',
-            description: 'El vaso termo es un recipiente aislado que mantiene las bebidas calientes o frías por más tiempo. Ideal para llevar tu café favorito contigo mientras te desplazas.',
-            price: '$30.000'
-        },
-        // Agrega más productos de la galería 2 aquí
-    ],
-
-    Alimentos: [
-        { 
-            name: 'Macarrones de Café (Caja x12)',
-            imgSrc: 'Proyecto-Final/img/Productos/Macarrones.png',
-            description: 'Deliciosos macarrones con sabor a café, una combinación perfecta de textura suave y el distintivo aroma del café.',
-            price: '$15.000'
-        },
-        { 
-            name: 'Frappuccino Mocha',
-            imgSrc: 'Proyecto-Final/img/Productos/Frappuccino.png',
-            description: 'Bebida de café lista para llevar, perfecta para aquellos momentos en los que necesitas tu dosis de cafeína de manera rápida y conveniente.',
-            price: '$8.000'
-        },
-        { 
-            name: 'Licor de café Coloma',
-            imgSrc: 'Proyecto-Final/img/Productos/Licor.png',
-            description: 'Un licor exquisito con el distintivo sabor del café. Perfecto para cócteles, postres o simplemente disfrutarlo solo.',
-            price: '$75.000'
-        },
-        { 
-            name: 'Arequipe de café',
-            imgSrc: 'Proyecto-Final/img/Productos/Arequipe.png',
-            description: 'Una receta original de dulce de leche con el toque de café excelso. Nuestro arequipe ofrece una experiencia suave al paladar, especial para repostería y salsa para helados. ',
-            price: '$13.000'
-        },
+    gallery3: [
+        { name: 'Product 5', imgSrc: 'img/gallery-5.jpg', description: 'Descripción del producto 5' },
+        { name: 'Product 6', imgSrc: 'img/gallery-6.jpg', description: 'Descripción del producto 6' },
         // Agrega más productos de la galería 3 aquí
-        
-    ],
-
-    Belleza: [
-        { 
-            name: 'Isolo Mascarilla Purificante', 
-            imgSrc: 'Proyecto-Final/img/Productos/Mascarilla.png',
-            description: 'Enriquecida con antioxidantes y propiedades exfoliantes naturales del café, esta mascarilla ayuda a eliminar impurezas, promover la circulación sanguínea y dejar la piel con un aspecto fresco y radiante.',
-            price: '$45.000'
-        },
-        { 
-            name: 'Jabón Corporal Tú Aroma', 
-            imgSrc: 'Proyecto-Final/img/Productos/Jabón.png',
-            description: 'Jabón enriquecido con café, conocido por sus propiedades exfoliantes y revitalizantes. Ideal para una experiencia de cuidado personal enriquecida con el aroma del café.',
-            price: '$10.000'
-        },
-        { 
-            name: 'Crema Reafirmante "Supréme"', 
-            imgSrc: 'Proyecto-Final/img/Productos/Crema.png',
-            description: 'Descubre la firmeza y elasticidad con nuestra crema corporal reafirmante de café. Enriquecida con extracto de café, esta crema ayuda a tonificar la piel, reducir la apariencia de la celulitis y mejorar la textura. Disfruta de una piel más suave y tersa con un aroma deliciosamente estimulante.', 
-            price: '$60.000' 
-        },
-        { 
-            name: 'Aceite Facial Revitalizante "Esencia Cafetera"', 
-            imgSrc: 'Proyecto-Final/img/Productos/Aceite.png',
-            description: 'Revitaliza tu rutina de cuidado facial con nuestro aceite facial de café. Formulado con aceites naturales y extracto de café, este elixir revitalizante ayuda a hidratar, nutrir y darle un impulso antioxidante a tu piel. Disfruta de una tez radiante y con un brillo saludable.', 
-            price: '$25.000'
-        }
-        // Agrega más productos de la galería 3 aquí
-    ],
-
-    Artesanías: [
-        {
-            name: 'Vela Aromática',
-            imgSrc: 'Proyecto-Final/img/Productos/Vela.png',
-            description: 'Ilumina tus espacios con la suave luz de nuestra vela aromática a base de café. Su fragancia cálida y acogedora te transportará a un rincón acogedor de una cafetería, creando un ambiente relajante y deliciosamente perfumado.',
-            price: '$25.000'
-          },
-          {
-            name: 'Aretes ',
-            imgSrc: 'Proyecto-Final/img/Productos/Aretes.png',
-            description: 'Descubre la elegancia en cada detalle con nuestros aretes hechos a mano con granos de café. Un accesorio único que combina la belleza natural del café con el arte de la joyería, creando una pieza que resalta tu estilo de manera sutil y sofisticada.',
-            price: '$50.000'
-          },
-          {
-            name: 'Escultura Elefante Caff',
-            imgSrc: 'Proyecto-Final/img/Productos/Escultura.png',
-            description: 'Sumérgete en la magia del arte con nuestra escultura de elefante elaborada completamente con granos de café. Cada pieza es una obra maestra única que captura la esencia y la majestuosidad de estos animales, aportando un toque distintivo y artístico a tu espacio.',
-            price: '$180.000'
-          },
-          {
-            name: 'Kit Siembra de Café',
-            imgSrc: 'Proyecto-Final/img/Productos/Matera.png',
-            description: 'Experimenta el ciclo completo del café con nuestro kit de siembra que incluye una matera artesanal elaborada con granos de café reciclados. Cultiva tus propios cafetos y observa cómo crecen en esta matera única, fusionando la pasión por el café con la naturaleza en tu hogar. Ideal para amantes del café y aficionados a la jardinería.',
-            price: '$80.000'
-          },
-          
-        // Agrega más productos de la galería 3 aquí
-        
     ]
 };
 
--- js/modal.js
+++ js/modal.js
@@ -4,39 +4,17 @@ import { Producto } from './objetos.js';
 //Crear los objetos para que se puedan llamar en la clase por medio de un array
 let productos = 
     [
-        //Todos los productos(?)
-        new Producto('Product 1', 'Descripción del producto 1', 'img/gallery-1.jpg', null, null),
-        new Producto('Product 2', 'Descripción del producto 2', 'img/gallery-2.jpg', null, null),
-    
-        //Cafés
-        new Producto('Café Mesa de los Santos', 'Granos de café de alta calidad, tipo arabica, listos para moler y preparar la taza perfecta de café en casa. Disfruta de la frescura y el sabor auténtico.', 'Proyecto-Final/img/Productos/Mesa.png', '20', '$25.000'),
-        new Producto('Café Premium "Arábica San Alberto"', 'Disfruta de la elegancia y el sabor refinado. Proveniente de las mejores cosechas, cada grano es seleccionado con cuidado para ofrecerte una experiencia única con notas florales, cuerpo sedoso y un toque de avellana.', 'Proyecto-Final/img/Productos/Arabica.png', '15', '$35.000'),
-        new Producto('Mezcla Intensa "Q.Coffee"', 'Sumérgete en la intensidad de nuestro café. Esta mezcla única ofrece un perfil de sabor audaz y fuerte, con notas de cacao y un toque ahumado. Ideal para aquellos que buscan una experiencia de café potente y llena de carácter.', 'Proyecto-Final/img/Productos/Q.png', '12', '$30.000'),
-        new Producto('Café Descafeinado "Aroma De Occidente"', 'Experimenta la tranquilidad con nuestro café descafeinado. Una mezcla suave y equilibrada, perfecta para disfrutar en cualquier momento del día. Conserva el delicioso sabor del café sin cafeína, con notas de almendra y un toque sutil de chocolate.', 'Proyecto-Final/img/Productos/Aroma.png', null, '$45.000'),
-
-        //Accesorios
-        new Producto('Aeropress (4 Tazas)', 'Innovadora herramienta para preparar café que combina la simplicidad y la calidad. Su diseño portátil la hace perfecta para llevarla contigo y disfrutar de tu café favorito en cualquier lugar.', 'Proyecto-Final/img/Productos/Aeropress.png', '4', '$160.000'),
-        new Producto('Mini Capuchinera', 'Una máquina compacta para preparar diferentes tipos de café. Ofrece la comodidad de disfrutar de un café de calidad de barista sin salir de casa.', 'Proyecto-Final/img/Productos/Capuccinera.png', '2', '$550.000'),
-        new Producto('Chemex Clásica (6 Tazas)', 'Elegante cafetera de vidrio para preparar café mediante el método de goteo. Su diseño único y la calidad del café que produce la convierten en una opción popular entre los amantes del café.', 'Proyecto-Final/img/Productos/Chemex.png', '3' , '$320.000'),
-        new Producto('Termo Vaso', 'El vaso termo es un recipiente aislado que mantiene las bebidas calientes o frías por más tiempo. Ideal para llevar tu café favorito contigo mientras te desplazas.', 'Proyecto-Final/img/Productos/Vaso.png', '40', '$30.000'),
-    
-        //Alimentos
-        new Producto('Macarrones de Café (Caja x12)', 'Deliciosos macarrones con sabor a café, una combinación perfecta de textura suave y el distintivo aroma del café.', 'Proyecto-Final/img/Productos/Macarrones.png', '20', '$15.000'),
-        new Producto('Frappuccino Mocha', 'Bebida de café lista para llevar, perfecta para aquellos momentos en los que necesitas tu dosis de cafeína de manera rápida y conveniente.', 'Proyecto-Final/img/Productos/Frappuccino.png', '15', '$8.000'),
-        new Producto('Licor de café Coloma', 'Un licor exquisito con el distintivo sabor del café. Perfecto para cócteles, postres o simplemente disfrutarlo solo.', 'Proyecto-Final/img/Productos/Licor.png', '5', '$75.000'),
-        new Producto('Arequipe de café', 'Una receta original de dulce de leche con el toque de café excelso. Nuestro arequipe ofrece una experiencia suave al paladar, especial para repostería y salsa para helados.', 'Proyecto-Final/img/Productos/Arequipe.png', '17', '$13.000'),
-    
-        //Belleza
-        new Producto('Isolo Mascarilla Purificante', 'Enriquecida con antioxidantes y propiedades exfoliantes naturales del café, esta mascarilla ayuda a eliminar impurezas, promover la circulación sanguínea y dejar la piel con un aspecto fresco y radiante.', 'Proyecto-Final/img/Productos/Mascarilla.png', '13', '$45.000'),
-        new Producto('Jabón Corporal Tú Aroma', 'Jabón enriquecido con café, conocido por sus propiedades exfoliantes y revitalizantes. Ideal para una experiencia de cuidado personal enriquecida con el aroma del café.', 'Proyecto-Final/img/Productos/Jabón.png', '20', '$10.000'),
-        new Producto('Crema Reafirmante "Supréme"', 'Descubre la firmeza y elasticidad con nuestra crema corporal reafirmante de café. Enriquecida con extracto de café, esta crema ayuda a tonificar la piel, reducir la apariencia de la celulitis y mejorar la textura. Disfruta de una piel más suave y tersa con un aroma deliciosamente estimulante.', 'Proyecto-Final/img/Productos/Crema.png', '10', '$60.000'),
-        new Producto('Aceite Facial Revitalizante "Esencia Cafetera"', 'Revitaliza tu rutina de cuidado facial con nuestro aceite facial de café. Formulado con aceites naturales y extracto de café, este elixir revitalizante ayuda a hidratar, nutrir y darle un impulso antioxidante a tu piel. Disfruta de una tez radiante y con un brillo saludable.', 'Proyecto-Final/img/Productos/Aceite.png', '21', '$25.000'),
-    
-        //Artesanías
-        new Producto('Vela Aromática', 'Ilumina tus espacios con la suave luz de nuestra vela aromática a base de café. Su fragancia cálida y acogedora te transportará a un rincón acogedor de una cafetería, creando un ambiente relajante y deliciosamente perfumado.', 'Proyecto-Final/img/Productos/Vela.png', '12', '$25.000'),
-        new Producto('Aretes ', 'Descubre la elegancia en cada detalle con nuestros aretes hechos a mano con granos de café. Un accesorio único que combina la belleza natural del café con el arte de la joyería, creando una pieza que resalta tu estilo de manera sutil y sofisticada.', 'Proyecto-Final/img/Productos/Aretes.png', '10', '$50.000'),
-        new Producto('Escultura Elefante Caff', 'Sumérgete en la magia del arte con nuestra escultura de elefante elaborada completamente con granos de café. Cada pieza es una obra maestra única que captura la esencia y la majestuosidad de estos animales, aportando un toque distintivo y artístico a tu espacio.', 'Proyecto-Final/img/Productos/Escultura.png', '1', '$180.000'),
-        new Producto('Kit Siembra de Café', 'Experimenta el ciclo completo del café con nuestro kit de siembra que incluye una matera artesanal elaborada con granos de café reciclados. Cultiva tus propios cafetos y observa cómo crecen en esta matera única, fusionando la pasión por el café con la naturaleza en tu hogar. Ideal para amantes del café y aficionados a la jardinería.', 'Proyecto-Final/img/Productos/Matera.png', '5', '$80.000'),
+        new Producto ('Café Mesa de los Santos', 'Granos de café de alta calidad, tipo arabica, listos para moler y preparar la taza perfecta de café en casa. Disfruta de la frescura y el sabor auténtico.', '/img/Productos/Santos.png', 20, 25000),
+        new Producto('Mezcla Intensa "Q.Coffee"', 'Sumérgete en la intensidad de nuestro café. Esta mezcla única ofrece un perfil de sabor audaz y fuerte, con notas de cacao y un toque ahumado. Ideal para aquellos que buscan una experiencia de café potente y llena de carácter.', '/img/Productos/premium.png', 12, 30000),
+        new Producto('Café Premium "Arábica San Alberto"', 'Disfruta de la elegancia y el sabor refinado. Proveniente de las mejores cosechas, cada grano es seleccionado con cuidado para ofrecerte una experiencia única con notas florales, cuerpo sedoso y un toque de avellana.', '/img/Productos/Arabica.png', 15, 35000),
+        new Producto('Café Descafeinado "Aroma De Occidente"', 'Experimenta la tranquilidad con nuestro café descafeinado. Una mezcla suave y equilibrada, perfecta para disfrutar en cualquier momento del día. Conserva el delicioso sabor del café sin cafeína, con notas de almendra y un toque sutil de chocolate.', '/img/Productos/Aroma.png', 20, 45000),
+        new Producto('Aeropress (4 Tazas)', 'Innovadora herramienta para preparar café que combina la simplicidad y la calidad. Su diseño portátil la hace perfecta para llevarla contigo y disfrutar de tu café favorito en cualquier lugar.', '/img/Productos/Aeropress.png', 4, 160000),
+        new Producto('Mini Capuchinera', 'Una máquina compacta para preparar diferentes tipos de café. Ofrece la comodidad de disfrutar de un café de calidad de barista sin salir de casa.', '/img/Productos/Capuccinera.png', 2, 550000),
+        new Producto('Chemex Clásica (6 Tazas)', 'Elegante cafetera de vidrio para preparar café mediante el método de goteo. Su diseño único y la calidad del café que produce la convierten en una opción popular entre los amantes del café.', '/img/Productos/Chemex.png', 3 , 320000),
+        new Producto('Termo Vaso', 'El vaso termo es un recipiente aislado que mantiene las bebidas calientes o frías por más tiempo. Ideal para llevar tu café favorito contigo mientras te desplazas.', '/img/Productos/Vaso.png', 40, 30000),
+        new Producto('Macarrones de Café (Caja x12)', 'Deliciosos macarrones con sabor a café, una combinación perfecta de textura suave y el distintivo aroma del café.', '/img/Productos/Macarrones.png', 20, 15000),
+        new Producto('Frappuccino Mocha', 'Bebida de café lista para llevar, perfecta para aquellos momentos en los que necesitas tu dosis de cafeína de manera rápida y conveniente.', '/img/Productos/Frappuccino.png', 15, 8000),
+        new Producto ('Licor de café Coloma', 'Un licor exquisito con el distintivo sabor del café. Perfecto para cócteles, postres o simplemente disfrutarlo solo.', '/img/Productos/Licor.png', 5, 75000),
     ];
 
 // Crear el modal
@@ -93,11 +71,20 @@ function crearModal(producto) {
     // Aquí se almacena de acuerdo al parametro enviado, va a mostrar el precio correspondiente
     precioImagen.innerHTML = `<strong>Precio: ${producto.precio}</strong>`;
 
-    //concatenar en el contenedorB, el título, descripción, cantidad, precio
+    // Creamos un button que va contener el agregar carrito al clic dado
+    let agregarCarrito = document.createElement('button');
+    //Aquí se almacena de acuerdo al parametro enviado, va a mostrar "Agregar al carrito"
+    agregarCarrito.innerHTML = `<strong> Agregar al Carrito </strong>`;
+
+
+     //concatenar en el contenedorB, el título, descripción, cantidad, precio
     contenedorTextos.appendChild(tituloImagen);
     contenedorTextos.appendChild(descripcionImagen);
     contenedorTextos.appendChild(cantidadProducto);
     contenedorTextos.appendChild(precioImagen);
+    contenedorTextos.appendChild(agregarCarrito);
+
+    
 
     //ContenedorPrincipal que va a tener la imagen y los texto en fila
     contenedorModal.appendChild(imagen);
@@ -148,4 +135,5 @@ else
     //En caso que las imagenes y los productos creados no sean los mismo, saldra error y no se ejecutara la función
     console.log("las imagenes no son las mismas que los productos ingresados");
 }
+
 
